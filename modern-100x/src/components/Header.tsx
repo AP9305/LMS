8338490072f0dcfeef4f82a980926027269ea379
ayush <ayushbajpai9304@gmail.com>
2025-06-21 15:18:06 +0530

    fix: remove unused function and deploy changes

--- modern-100x/src/components/Header.tsx
+++ modern-100x/src/components/Header.tsx
@@ -6,17 +6,6 @@ if (typeof window !== 'undefined') {
   localStorage.setItem('theme', 'dark');
 }
 
-const toggleDarkMode = () => {
-  const html = document.documentElement;
-  if (html.classList.contains('dark')) {
-    html.classList.remove('dark');
-    localStorage.setItem('theme', 'light');
-  } else {
-    html.classList.add('dark');
-    localStorage.setItem('theme', 'dark');
-  }
-};
-
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
